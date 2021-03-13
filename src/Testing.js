8f982251b340220493738b55c9de71a656098c05
Meryem Gençali <merymgencc@gmail.com>
2021-03-13 18:43:26 +0300

    React Shop Project W/JohnSmilga

---
+++ src/Testing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Testing = () => {
+  return (
+    <Wrapper>
+      <h3>hello</h3>
+      <p>heloooooooo</p>
+      <button>click</button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section``;
+
+export default Testing;
